06b17e83ff56bc6d5f837b8e7237ea05b761d17c
Jordan Campbell <danielmartin1205@gmail.com>
2019-10-11 21:01:35 -0400

    what to test..

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import App from './App';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, waitForElement } from '@testing-library/react';
+import axiosMock from 'axios';
 
 afterEach(rtl.cleanup);
 
@@ -18,3 +20,15 @@ test('Render Dark Mode Switch', () => {
   expect(element).toBeVisible();
 })
 
+// test('Axios Call', async () => {
+// const wrapper = rtl.render(<App />);
+// const url = 'http://localhost:5000/api/players';
+// axiosMock.get.mockResolvedValueOnce({
+//   data: { value: 'hello'}
+// })
+// fireEvent.load(wrapper);
+
+//   const greetingNode  = await waitForElement(() => {
+    
+//   })
+// })
